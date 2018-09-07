4e9d82e9d7171b9f9facbd97bee5b3b86ebc162d
Joel Wasserman <joel.wasserman8@gmail.com>
2018-09-06 20:00:19 -0700

    help me

--- src/set1ch4.ts
+++ src/set1ch4.ts
@@ -2,25 +2,21 @@ import fs = require('fs')
 import readline = require('readline')
 import { singleByteXOR } from './set1ch3'
 
-function findEnglishLineInFile(filePath: string) {
+function findEnglishLineInFile(fileString: string) {
     let minScore = 1000000
     let minString = ''
-    const instream = fs.createReadStream(filePath)
-    const outstream = new (require('stream'))()
-    const rl = readline.createInterface(instream, outstream)
-    let closed = false
 
-    rl.on('line', line => {
+    const lines = fileString.split('/n')
+    for (var i = 0; i < lines.length; i++) {
+        const line = lines[i]
         const [resultScore, resultString] = singleByteXOR(line)
         if (resultScore < minScore) {
             minScore = resultScore
             minString = resultString
         }
-    })
+    }
 
-    rl.on('close', () => {
-        return minString
-    })
+    return minString
 }
 
 export {
--- src/test/set1ch4.test.ts
+++ src/test/set1ch4.test.ts
@@ -1,7 +1,15 @@
 import * as ava from 'ava'
+import * as fs from 'fs'
+import * as path from 'path';
 import { findEnglishLineInFile } from '../set1ch4'
 
 ava.test('find the english from text file single char xor\'d', t => {
-    console.log('helo', findEnglishLineInFile('./files/set1ch4text.txt'))
-    t.pass()
+    console.log('hi')
+    fs.readFile(path.join(__dirname, '../../files/set1ch4text.txt'), 'hex', (err, data) => {
+        console.log('in here?')
+        console.log(err)
+        console.log(data)
+        // console.log('helo', findEnglishLineInFile(data.toString('hex')))
+        // t.pass()
+    })
 })
