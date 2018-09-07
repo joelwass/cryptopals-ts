d81968b55d37e10a9c6d295e897118a75a2c6e6d
Joel Wasserman <joel.wasserman8@gmail.com>
2018-09-07 11:12:10 -0700

    update sad

--- src/set1ch4.ts
+++ src/set1ch4.ts
@@ -6,21 +6,17 @@ async function findEnglishLineInFile(fileString: string): Promise<any> {
     let minScore = 1000000
     let minString = ''
 
-    const lines = fileString.split('/n')
+    const lines = fileString.split('\n')
     for (var i = 0; i < lines.length; i++) {
         const line = lines[i]
         const [resultScore, resultString] = await singleByteXOR(line)
-        console.log(resultString)
         if (resultScore < minScore) {
             minScore = resultScore
             minString = resultString
         }
-        if (i === lines.length -1) {
-            return Promise.resolve(minString)
-        }
     }
 
-    // return Promise.resolve(minString)
+    return Promise.resolve(minString)
 }
 
 export {
