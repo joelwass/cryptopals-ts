83c718ce449f00d2449018cfdd8b02c0a3ae703e
Joel Wasserman <joel.wasserman8@gmail.com>
2018-09-07 07:15:12 -0700

    working on ch4

--- src/test/set1ch4.test.ts
+++ src/test/set1ch4.test.ts
@@ -4,12 +4,7 @@ import * as path from 'path';
 import { findEnglishLineInFile } from '../set1ch4'
 
 ava.test('find the english from text file single char xor\'d', t => {
-    console.log('hi')
-    fs.readFile(path.join(__dirname, '../../files/set1ch4text.txt'), 'hex', (err, data) => {
-        console.log('in here?')
-        console.log(err)
-        console.log(data)
-        // console.log('helo', findEnglishLineInFile(data.toString('hex')))
-        // t.pass()
-    })
+    const textContents = fs.readFileSync(path.join(__dirname, '../../files/set1ch4text.txt'), { encoding: 'ascii' })
+    console.log('helo', findEnglishLineInFile(textContents))
+    t.fail()
 })
