56cde186a36b1cbb616bf8fec9304785d5527ace
Joel Wasserman <joel.wasserman8@gmail.com>
2018-09-16 20:35:02 -0700

    update

--- src/set1/set1ch7.ts
+++ src/set1/set1ch7.ts
@@ -2,11 +2,10 @@ import * as crypto from 'crypto'
 import { addPKCS7Padding } from '../set2/set2ch9'
 
 function decryptAES128InECB(data: Buffer, key: Buffer): Buffer {
-    const paddedData: Buffer = addPKCS7Padding(data, 16)
     const decipher = crypto.createDecipheriv('aes-128-ecb', key, Buffer.alloc(0))
     decipher.setAutoPadding(false)
     return Buffer.concat([
-        decipher.update(paddedData),
+        decipher.update(data),
         decipher.final()
     ])
 }
