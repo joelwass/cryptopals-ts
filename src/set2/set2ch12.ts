d594fea435a608c0cfd9cd9294b032d1f9d41aae
Joel Wasserman <joel.wasserman8@gmail.com>
2018-09-21 08:30:39 -0700

    fixed ecb cbc detection test

--- src/set2/set2ch12.ts
+++ src/set2/set2ch12.ts
@@ -4,9 +4,9 @@ import { detectAESinECBMode } from '../set1/set1ch8'
 
 const key = genRandomAESKey(16)
 
-function byteAtaTimeECBDecryption(unknownString: Buffer) {
+async function byteAtaTimeECBDecryption(unknownString: Buffer) {
 
-    const initialEncrypted: Buffer = serverEncryption(Buffer.alloc(0), unknownString)
+    const initialEncrypted: Buffer = await serverEncryption(Buffer.alloc(0), unknownString)
     const initialLength: number = initialEncrypted.length
     let blockSize: number
 
@@ -15,7 +15,7 @@ function byteAtaTimeECBDecryption(unknownString: Buffer) {
         let str = 'A'.repeat(i)
         let myStringBuff = Buffer.from(str, 'ascii')
 
-        let encrypted = serverEncryption(myStringBuff, unknownString)
+        let encrypted = await serverEncryption(myStringBuff, unknownString)
         if (encrypted.length !== initialLength) {
             // block size has increased by block size amount, so let's find block size
             blockSize = encrypted.length - initialLength
@@ -25,11 +25,33 @@ function byteAtaTimeECBDecryption(unknownString: Buffer) {
 
     // detect ecb or cbc
     const isECB = detectAESinECBMode(unknownString, blockSize)
+    let knownValues = ''
+
+    // iterate over each letter in blocksize 
+    for (let i = 1; i <= 1; i++ {
+        // pass in a string of length block size - 1
+        const ourString = 'A'.repeat(blockSize - i)
+
+        // capture output from server of blocksize - i
+        const serverOutput = await serverEncryption(Buffer.from(ourString, 'ascii'), unknownString)
+
+        // iterate over all char codes
+        for (let j = 0; j < 256; j++) {
+            const serverTestInput = ourString + String.fromCharCode(j) + knownValues
+            const serverTestOutput = await serverEncryption(Buffer.from(serverTestInput), unknownString)
+            if (serverTestOutput == serverOutput) {
+                knownValues = String.fromCharCode(j) + knownValues
+                console.log(knownValues)
+            }
+        }
+    }
+
+
 }
 
-function serverEncryption(data: Buffer, unknownString: Buffer): Buffer {
+function serverEncryption(data: Buffer, unknownString: Buffer): Promise<Buffer> {
     const totalData = Buffer.concat([data, unknownString], data.length + unknownString.length)
-    return encryptAES128InECB(totalData, key)
+    return Promise.resolve(encryptAES128InECB(totalData, key))
 }
 
 
